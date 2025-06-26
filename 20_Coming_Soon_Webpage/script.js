736a884b2e590c3d95bb86438aacdc075ad013b8
nvedant382 <nvedant382@gmail.com>
2025-06-27 04:37:26 +0530

    Added CountDown Page

---
+++ 20_Coming_Soon_Webpage/script.js
@@ -0,0 +1,16 @@
+let countDownDate = new Date("Nov 27,2025 00:00:00").getTime()
+
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}, 1000)
